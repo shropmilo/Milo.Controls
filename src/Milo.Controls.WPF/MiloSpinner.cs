827ea9e5256c71f28dce1ea75d2671c56988f915
Miles Hill <Miles.Hill@mail.nidec.com>
2025-04-27 10:01:13 +0100

    Bootstrapper

---
+++ src/Milo.Controls.WPF/MiloSpinner.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Milo.Controls.WPF
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public class MiloSpinner : Control
+    {
+        static MiloSpinner()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MiloSpinner), new FrameworkPropertyMetadata(typeof(MiloSpinner)));
+        }
+    }
+}
